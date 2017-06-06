425f2083715c4077dbbf12accc1595fb2adc691b
Raghav Mittal <you@example.com>
2017-06-06 12:23:03 +0530

    vectorisation done

--- tf-idf.py
+++ tf-idf.py
@@ -1,2 +1,14 @@
-def getTF(productList, query):
 
+def getVector(document):
+    wordArr = document.split(" ")
+    wordList = {}
+    for word in wordArr:
+        if word in wordList:
+            wordList[word] = wordList[word] + 1
+        else:
+            wordList[word] = 1
+    n = len(wordList)
+    print n
+    return wordList
+
+getVector('hey rghav dude dude jbkrj')
