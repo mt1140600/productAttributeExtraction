0949f1cd03e122e1e1ec052396c0bc5079e20d17
Raghav Mittal <raghav.mittal1418@gmail.com>
2017-06-16 00:10:34 +0530

    scripting done

--- test.py
+++ test.py
@@ -8,38 +8,39 @@ def load_model(file_name):
     with open(file_name, 'rb') as fid:
         model = pickle.load(fid)
     return model
-from pymongo import MongoClient
-client = MongoClient('10.240.0.9')
-db = client.prokure2
-pincodedetails = db.productfinals
+# from pymongo import MongoClient
+# client = MongoClient('10.240.0.9')
+# db = client.prokure2
+# pincodedetails = db.productfinals
 brands=[]
+types = []
+subcategory = []
 i=0
-x = []
-for row in pincodedetails.find():
-    x.append(row)
-    print
-    print
-    i=i+1
-    if i>10:
-        break
-save_model('details.pkl', x)
-# with open('newFile.csv','w') as svfile:
-#     spmwriter = csv.writer(svfile)
-#     with open('products.csv') as ifile:
-#         read = csv.reader(ifile)
-#         for row in read:
-#             x = str(row[0])
-#             if 'back' in x.lower():
-#                 spmwriter.writerow([x])
-    # print
-# print brands
-# for i in range(n):
-#     print 'Brand: '+str(brands[i])
-#     with open('products.csv') as ifile:
-#         read = csv.reader(ifile)
-#         for row in read:
-#             x = str(row[0])
-#             if str(brands[i]).lower() in x.lower():
-#                 print x
-#     print
+# for row in pincodedetails.find():
+#     x.append(row)
+#     i = i+1
+#     print i
+#     # if i==101:
+#     #     break
+import csv
+b = load_model('details.pkl')
+for row in b:
+    if row['subCategoryFilters']['Brand'] not in brands:
+        brands.append(row['subCategoryFilters']['Brand'])
+    if row['subCategory'] not in subcategory:
+        subcategory.append(row['subCategory'])
 
+typeMap = {}
+for cat in subcategory:
+    arr = []
+    for row in b:
+        x = row['subCategoryFilters']
+        if 'Type' in x and (x['Type'])[0] not in arr:
+            arr.append((x['Type'])[0])
+    typeMap[cat[0]] = arr
+
+# print types
+for brand in brands:
+    for cat in subcategory:
+        for t in typeMap[cat[0]]:
+            print brand[0]+' '+t+' '+cat[0]
